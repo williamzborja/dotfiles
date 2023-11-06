fa33a97d348995000e9bd6cdd5ffd0c84be9d2c8
williamzborja <williamzborja@gmail.com>
2023-11-06 14:25:45 -0500

    add nix files

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # remove and set
 
 function set_dotfile(){
 	local DST=$1
 	local FILE_NAME=$2
-	rm -rf "$DST/$FILE_NAME"
+	sudo rm -rf "$DST/$FILE_NAME"
 	ln -s "$(pwd)/$FILE_NAME" "$DST/$FILE_NAME"
 #	source "$DST/$FILE_NAME" 
 }
@@ -13,11 +13,12 @@ function set_dotfile(){
 CONFIG="$HOME/.config"
 
 set_dotfile $HOME ".aliases"
-set_dotfile $HOME ".tmux.conf"
+#set_dotfile $HOME ".tmux.conf"
 set_dotfile $HOME ".config/alacritty"
-set_dotfile $HOME ".config/starship.toml"
+#set_dotfile $HOME ".config/starship.toml"
 set_dotfile $HOME ".config/nvim"
-set_dotfile $HOME ".config/kitty"
+set_dotfile "/etc" "nixos" 
+#set_dotfile $HOME ".config/kitty"
 
-set_dotfile $HOME ".zshrc"
+#set_dotfile $HOME ".zshrc"
 
