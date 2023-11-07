5f7c05f492f633af3e081bc7b40d37411990fc12
williamzborja <williamzborja@gmail.com>
2023-11-07 16:45:49 -0500

    changing to gnome and clang

--- nixos/system_packages.nix
+++ nixos/system_packages.nix
@@ -1,11 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      wget
-     gcc13
+     clang 
      gnumake
      git
      fzf
--- nixos/xserver.nix
+++ nixos/xserver.nix
@@ -1,15 +1,27 @@
 { config, pkgs, ... }:
 
 {
+
+environment.gnome.excludePackages = (with pkgs; [
+      gnome-photos
+      gnome-tour
+    ]);
+
 # Configure keymap in X11
   services.xserver = {
     enable = true;
     layout = "us";
     xkbVariant = "";
 
-    windowManager.dwm.enable = true;
+#    windowManager.dwm.enable = true;
 
+    desktopManager.gnome.enable = true;
+    
     displayManager = {
+
+      gdm = {
+        enable = true;
+      };
       autoLogin.enable = true;
       autoLogin.user = "dev";
     };
